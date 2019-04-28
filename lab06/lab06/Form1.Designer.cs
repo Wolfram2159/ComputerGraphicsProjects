e47ef025d4693a4d3a2c653649e048a45db1b7bc
jakub215900 <lol215900>
2019-04-29 00:42:09 +0200

    lab06 finished

---
+++ lab06/lab06/Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace lab06
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(507, 331);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(713, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Hermit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(713, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Bezier";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(713, 70);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Bezier 2";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(668, 203);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 4;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(582, 99);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(688, 99);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(582, 125);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(688, 125);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 8;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(582, 151);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 9;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(688, 151);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 10;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(582, 177);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 20);
+            this.textBox7.TabIndex = 11;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(688, 177);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(100, 20);
+            this.textBox8.TabIndex = 12;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(549, 102);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "P1: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(549, 128);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "P4: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(549, 154);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(27, 13);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "R1: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(549, 180);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(24, 13);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "R4:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ lab06/lab06/Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab06
+{
+    public partial class Form1 : Form
+    {
+        private System.Drawing.Graphics g;
+        private System.Drawing.Pen pen1 = new System.Drawing.Pen(Color.Blue, 3);
+        private System.Drawing.Pen pen2 = new System.Drawing.Pen(Color.Red, 3);
+        public Form1()
+        {
+            InitializeComponent();
+            g = pictureBox1.CreateGraphics();
+            numericUpDown1.Value = 100;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        public float returnFloat(string text)
+        {
+            return float.Parse(text);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Refresh();
+            int n = GetPointsAmount();
+
+            var p1 = new PointF(returnFloat(textBox1.Text), returnFloat(textBox2.Text));//new PointF(100, 100);
+            var r1 = new PointF(returnFloat(textBox3.Text), returnFloat(textBox4.Text));//new PointF(500, 500);
+            var p4 = new PointF(returnFloat(textBox5.Text), returnFloat(textBox6.Text));//new PointF(300, 100);
+            var r4 = new PointF(returnFloat(textBox7.Text), returnFloat(textBox8.Text));//new PointF(500, 500);
+
+            g.DrawLines(pen1, DrawHermite(p1, p4, r1, r4, n));
+            //g.DrawLine(pen2, p1.X, p1.Y, p1.X + r1.X, p1.Y + r1.Y); //wektor r1
+            //g.DrawLine(pen2, p4.X, p4.Y, p4.X + r4.X, p4.Y + r4.Y); // wektor r4
+        }
+
+        private PointF[] DrawHermite(PointF p1, PointF p4, PointF r1, PointF r4, int amount)
+        {
+            double currentT = 0;
+            double deltaT = 1d / amount;
+            PointF[] points = new PointF[amount];
+            for (int i = 0; i < amount; i++)
+            {
+                double x = CalcHermite(p1.X, p4.X, r1.X, r4.X, currentT);
+                double y = CalcHermite(p1.Y, p4.Y, r1.Y, r4.Y, currentT);
+                points[i] = new PointF((float)x, (float)y);
+                currentT += deltaT;
+            }
+
+            return points;
+        }
+
+        private PointF[] DrawBezier(PointF p1, PointF p2, PointF p3, PointF p4, int amount)
+        {
+            double currentT = 0;
+            double deltaT = 1d / amount;
+            PointF[] points = new PointF[amount];
+            for (int i = 0; i < amount; i++)
+            {
+                double x = CalcBezier(p1.X, p2.X, p3.X, p4.X, currentT);
+                double y = CalcBezier(p1.Y, p2.Y, p3.Y, p4.Y, currentT);
+                points[i] = new PointF((float)x, (float)y);
+                currentT += deltaT;
+            }
+
+            return points;
+        }
+
+        private PointF[] DrawBSpline(PointF[] refPoints, int amount)
+        {
+            double currentT = 0;
+            double deltaT = 1d / amount;
+            PointF[] points = new PointF[amount * (refPoints.Length - 3)];
+
+
+            for (int i = 0; i < refPoints.Length - 3; i++)
+            {
+
+                for (int j = 0; j < amount; j++)
+                {
+                    double x = CalcBSpline(refPoints[i].X, refPoints[i + 1].X, refPoints[i + 2].X, refPoints[i + 3].X, currentT);
+                    double y = CalcBSpline(refPoints[i].Y, refPoints[i + 1].Y, refPoints[i + 2].Y, refPoints[i + 3].Y, currentT);
+
+                    points[j] = new PointF((float)x, (float)y);
+                    currentT += deltaT;
+                }
+                currentT = 0;
+
+            }
+            return points;
+        }
+
+        private double CalcHermite(double p1, double p4, double r1, double r4, double t)
+        {
+            double tPow3 = Math.Pow(t, 3);
+            double tPow2 = Math.Pow(t, 2);
+
+            return (2 * tPow3 - 3 * tPow2 + 1) * p1 + (-2 * tPow3 + 3 * tPow2) * p4
+                   + (tPow3 - 2 * tPow2 + t) * r1 + (tPow3 - tPow2) * r4;
+        }
+
+        private double CalcBezier(double p1, double p2, double p3, double p4, double t)
+        {
+            return Math.Pow((1 - t), 3) * p1
+                   + 3 * t * Math.Pow((1 - t), 2) * p2
+                   + 3 * t * t * (1 - t) * p3
+                   + Math.Pow(t, 3) * p4;
+        }
+
+        //p1 => P (i-3)
+        //p4 => P (i)
+        private double CalcBSpline(double p1, double p2, double p3, double p4, double t)
+        {
+            return (Math.Pow(1 - t, 3) / 6d) * p1
+                   + ((3 * Math.Pow(t, 3) - 6 * t * t + 4) / 6d) * p2
+                   + (-3 * Math.Pow(t, 3) + 3 * Math.Pow(t, 2) + 3 * t + 1) / 6d * p3
+                   + Math.Pow(t, 3) / 6d * p4;
+        }
+
+        private int GetPointsAmount()
+        {
+            return Convert.ToInt32(numericUpDown1.Value);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Refresh();
+            int n = GetPointsAmount();
+
+            var p1 = new PointF(returnFloat(textBox1.Text), returnFloat(textBox2.Text));//new PointF(100, 100);
+            var p2 = new PointF(returnFloat(textBox3.Text), returnFloat(textBox4.Text));//new PointF(500, 500);
+            var p3 = new PointF(returnFloat(textBox5.Text), returnFloat(textBox6.Text));//new PointF(300, 100);
+            var p4 = new PointF(returnFloat(textBox7.Text), returnFloat(textBox8.Text));//new PointF(500, 500);
+            //var p1 = new PointF(100, 100);
+            //var p2 = new PointF(150, 10);
+            //var p3 = new PointF(250, 190);
+            //var p4 = new PointF(300, 100);
+
+
+            g.DrawLines(pen1, DrawBezier(p1, p2, p3, p4, n));
+
+            float r = 10;
+
+            //g.DrawEllipse(pen2, p2.X - r, p2.Y - r, 2 * r, 2 * r);
+            //g.DrawEllipse(pen2, p3.X - r, p3.Y - r, 2 * r, 2 * r);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Refresh();
+            int n = GetPointsAmount();
+
+            var p1 = new PointF(100, 100);
+            var p2 = new PointF(150, 500);
+            var p3 = new PointF(250, 600);
+            var p4 = new PointF(600, 100);
+            var p5 = new PointF(150, 200);
+            var p6 = new PointF(50, 220);
+            var p7 = new PointF(900, 600);
+
+            PointF[] tab = { p1, p2, p3, p4, p5, p6, p7 };
+
+
+            g.DrawLines(pen1, DrawBSpline(tab, n));
+        }
+    }
+}
